7903e97c5b31a89d3e23fff455325f75014684cf
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-11-17 13:36:25 +0100

    añadir aviones a lista

--- src/main/java/cat/samu/Main.java
+++ src/main/java/cat/samu/Main.java
@@ -1,5 +1,6 @@
 package cat.samu;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,12 +8,20 @@ import java.util.Scanner;
 public class Main {
 
     static Scanner sc = new Scanner(System.in);
+    static ArrayList<Avion> aviones = new ArrayList<>();
 
     static void main() {
         try {
             int nAviones = Airport.askAviones();
             int nPistas = Airport.askPistas();
             System.out.println(nAviones + " " + nPistas);
+
+            for(int i = 0; i < nAviones; i++){
+                aviones.add(new Avion("IB000"+i, Estado.EN_ESPERA));
+                System.out.println();
+            }
+            System.out.println(nAviones + " " + nPistas);
+
         }catch(Exception e) {
             System.out.println(e.getMessage());
         }
