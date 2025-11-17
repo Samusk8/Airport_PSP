c3506ac93606b119fe2da63a7290ab57ebf744cb
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-11-17 11:08:36 +0100

    Pistas y aviones

--- src/main/java/cat/samu/Main.java
+++ src/main/java/cat/samu/Main.java
@@ -1,17 +1,58 @@
 package cat.samu;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    static Scanner sc = new Scanner(System.in);
+
+    static void main() {
+        try {
+            int nAviones = askAviones();
+            int nPistas = askPistas();
+            System.out.println(nAviones + " " + nPistas);
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    static int askAviones(){
+        int nAviones = 0;
+        boolean continua = true;
+        while(continua){
+            System.out.println("Ingresa el número de aviones");
+            System.out.println("(min 10)");
+            sc = new Scanner(System.in);
+            nAviones = sc.nextInt();
+
+
+            if  (nAviones >= 10 ) {
+
+                continua = false;
+            }
+        }
+        return nAviones;
+
+    }
+
+    static int askPistas(){
+        int nPistas = 0;
+        boolean continua = true;
+        while(continua){
+            System.out.println("Ingresa el número de aviones");
+            System.out.println("(min 2)");
+            sc = new Scanner(System.in);
+            nPistas = sc.nextInt();
+
+
+            if  (nPistas >= 2 ) {
+
+                continua = false;
+            }
+        }
+        return nPistas;
+
     }
 }
