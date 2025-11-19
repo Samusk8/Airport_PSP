bd5f7e84801c011f39a627f451a8ba9235d16115
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-11-19 18:38:39 +0100

    torreControl
    Version1 ahora la pruebo 🙏

--- src/main/java/cat/samu/TorreControl.java
+++ src/main/java/cat/samu/TorreControl.java
@@ -1,15 +1,55 @@
 package cat.samu;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TorreControl {
-    private int nPistas;
+    //private int nPistas;
+    private ReentrantLock[] pistas;
+    private boolean[] pistaslibres;
+
+    private ReentrantLock controlLock = new ReentrantLock();
+
 
     public TorreControl(int nPistas) {
-        this.nPistas = nPistas;
+        //this.nPistas = nPistas;
+        pistas  = new ReentrantLock[nPistas];
+        pistaslibres = new boolean[nPistas];
+        for(int i = 0; i < nPistas; i++){
+            pistas[i] = new ReentrantLock();
+            pistaslibres[i] = true;
+        }
     }
-    public int getNPistas() {
-        return nPistas;
+
+    public int solicitarAterrizaje(){
+        while(true){
+            controlLock.lock();
+            try{
+                int pista = buscarpistaLibre();
+                if (pista >= 0){
+                    pistaslibres[pista] = false;
+                    return pista;
+                }
+            } finally{
+                controlLock.unlock();
+            }
+        }
     }
-    public void setNPistas(int nPistas) {
-        this.nPistas = nPistas;
+
+    public void liberarPista(int pista){
+        controlLock.lock();
+        try{
+            pistaslibres[pista] = true;
+        } finally{
+            controlLock.unlock();
+        }
+    }
+
+    public int buscarpistaLibre(){
+        for (int i = 0; i < pistaslibres.length; i++) {
+            if(pistaslibres[i] == true){
+                return i;
+            }
+        }
+        return -1;
     }
 }
